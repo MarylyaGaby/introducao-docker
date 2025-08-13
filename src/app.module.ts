2bf515dd2474853190856a6c9d191d6fa1cf39e1
Marilia Gabriele <mariliagabriele081@gmail.com>
2025-08-13 18:18:35 -0300

    fix: fix project dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
+import { Module } from '@nestjs/common';
+import { BookModule } from './books/book.module';
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BookModule, PrismaModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/books/book.service.ts
@@ -0,0 +1,82 @@
+import { ConflictException, Injectable, NotFoundException } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { CreateBookDto } from "./dto/create-book.dto";
+import { UpdateBookDto } from "./dto/update-book.dto";
+import { Book } from "@prisma/client";
+import { BookCategory } from '@prisma/client';
+
+@Injectable()
+export class BookService {
+    constructor(private prisma: PrismaService) { }
+
+    async create(data: CreateBookDto) {
+        const bookExist = await this.prisma.book.findUnique({
+            where: { title: data.title }
+        })
+
+        if (bookExist) {
+            throw new ConflictException("Livro já cadastrado!")
+        }
+
+        const newBook = await this.prisma.book.create({
+            data: {
+                title: data.title,
+                author: data.author,
+                publicationDate: data.publicationDate,
+                category: data.category?.toUpperCase() as BookCategory
+            }
+        })
+
+        return newBook
+    }
+
+    async findAll(): Promise<Book[]> {
+        return this.prisma.book.findMany()
+    }
+
+    async findById(id: string): Promise<Book | null> {
+        const founderBook = await this.prisma.book.findUnique({
+            where: { id }
+        })
+
+        if (!founderBook) {
+            throw new NotFoundException(`
+                
+                Livro com id ${id} não identificado
+                
+                `)
+        }
+
+        return founderBook
+    }
+
+    async update(id: string, data: UpdateBookDto): Promise<Book | null> {
+        try {
+            return await this.prisma.book.update({
+                where: { id },
+                data: {
+                    ...data,
+                    category: data.category ? { set: data.category as BookCategory } : undefined,
+                },
+            });
+        } catch {
+            throw new NotFoundException(`
+
+                Livro com id ${id} não identificado
+                
+                `)
+        }
+    }
+
+    async delete(id: string): Promise<Book | null> {
+        try {
+            return await this.prisma.book.delete({ where: { id } })
+        } catch {
+            throw new NotFoundException(`
+
+                Livro com id ${id} não identificado
+                
+                `)
+        }
+    }
+}
